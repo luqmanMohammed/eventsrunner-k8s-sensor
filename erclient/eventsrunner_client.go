a8ef18ac008acb1b87ee317106f150fc2587eef3
luqmanMohammed <m.luqman077@gmail.com>
2022-02-02 23:28:49 +0530

    Add ERClient with MTLS auth

---
+++ erclient/eventsrunner_client.go
@@ -0,0 +1,89 @@
+package erclient
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/luqmanMohammed/eventsrunner-k8s-sensor/sensor/eventqueue"
+)
+
+type EventsRunnerClientOpts struct {
+	CaCertPath     string
+	ClientKeyPath  string
+	ClientCertPath string
+	RequestTimeout time.Duration
+	JWTToken       string
+}
+
+type EventsRunnerClient struct {
+	eventsRunnerURI string
+	httpClient      *http.Client
+	headers         map[string]string
+}
+
+func NewMTLSClient(erURI string, erClientOpts EventsRunnerClientOpts) (*EventsRunnerClient, error) {
+	caCert, err := ioutil.ReadFile(erClientOpts.CaCertPath)
+	if err != nil {
+		return nil, err
+	}
+	caCertPool := x509.NewCertPool()
+	if ok := caCertPool.AppendCertsFromPEM(caCert); !ok {
+		return nil, errors.New("failed to setup tls config")
+	}
+	clientCert, err := tls.LoadX509KeyPair(erClientOpts.ClientCertPath, erClientOpts.ClientKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	httpClient := &http.Client{
+		Timeout: erClientOpts.RequestTimeout,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs:      caCertPool,
+				Certificates: []tls.Certificate{clientCert},
+			},
+		},
+	}
+
+	headers := make(map[string]string)
+	if erClientOpts.JWTToken != "" {
+		headers["Authorization"] = "Bearer " + erClientOpts.JWTToken
+	}
+	headers["Content-Type"] = "application/json"
+
+	return &EventsRunnerClient{
+		eventsRunnerURI: erURI,
+		httpClient:      httpClient,
+		headers:         headers,
+	}, nil
+}
+
+func (er EventsRunnerClient) ProcessEvent(event *eventqueue.Event) error {
+	eventJson, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", er.eventsRunnerURI, bytes.NewBuffer(eventJson))
+	if err != nil {
+		return err
+	}
+	for k, v := range er.headers {
+		req.Header.Set(k, v)
+	}
+	resp, err := er.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	fmt.Println("response Status:", resp.StatusCode)
+	if resp.StatusCode < 200 && resp.StatusCode >= 300 {
+		fmt.Println("came")
+		return errors.New("failed to process event")
+	}
+	return nil
+}
---
+++ erclient/eventsrunner_client_test.go
@@ -0,0 +1,138 @@
+package erclient
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/luqmanMohammed/eventsrunner-k8s-sensor/sensor/eventqueue"
+	"github.com/luqmanMohammed/eventsrunner-k8s-sensor/sensor/rules"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+var (
+	test_event = &eventqueue.Event{
+		EventType: rules.ADDED,
+		RuleID:    "test-rule",
+		Objects: []*unstructured.Unstructured{
+			{
+				Object: map[string]interface{}{
+					"apiVersion": "v1",
+					"kind":       "Pod",
+					"metadata": map[string]interface{}{
+						"name": "test-pod",
+					},
+				},
+			},
+		},
+	}
+
+	test_ca_pki_cert_script = `#!/bin/bash
+set -xe
+mkdir -p /tmp/test-pki
+
+cat <<EOF> /tmp/test-pki/csr.conf
+[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+
+[ dn ]
+C = US
+ST = CA
+L = CA
+O = test
+OU = test
+CN = 127.0.0.1
+
+[ req_ext ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = localhost
+DNS.2 = *.cluster.local
+IP.1 = 127.0.0.1
+
+[ v3_ext ]
+authorityKeyIdentifier=keyid,issuer:always
+basicConstraints=CA:FALSE
+keyUsage=keyEncipherment,dataEncipherment
+extendedKeyUsage=serverAuth,clientAuth
+subjectAltName=@alt_names
+EOF
+
+openssl genrsa -out /tmp/test-pki/ca.key 2048
+openssl req -x509 -new -nodes -key /tmp/test-pki/ca.key -subj "/CN=127.0.0.1" -days 10000 -out /tmp/test-pki/ca.crt
+
+openssl genrsa -out /tmp/test-pki/server.key 2048
+openssl req -new -key /tmp/test-pki/server.key -out /tmp/test-pki/server.csr -config /tmp/test-pki/csr.conf
+openssl x509 -req -in  /tmp/test-pki/server.csr -CA /tmp/test-pki/ca.crt -CAkey /tmp/test-pki/ca.key -CAcreateserial -out /tmp/test-pki/server.crt -days 10000 -extensions v3_ext -extfile /tmp/test-pki/csr.conf
+
+openssl genrsa -out /tmp/test-pki/client.key 2048
+openssl req -new -key /tmp/test-pki/client.key -out /tmp/test-pki/client.csr -subj "/CN=Client"
+openssl x509 -req -in  /tmp/test-pki/client.csr -CA /tmp/test-pki/ca.crt -CAkey /tmp/test-pki/ca.key -CAcreateserial -out /tmp/test-pki/client.crt -days 10000
+
+`
+)
+
+func setupTLS() error {
+	cmd := exec.Command("sh", "-c", test_ca_pki_cert_script)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func setupSetupMockServer(clientAuth tls.ClientAuthType) *http.Server {
+	caCert, err := ioutil.ReadFile("/tmp/test-pki/ca.crt")
+	if err != nil {
+		panic("Failed to read CA cert:" + err.Error())
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	tlsConfig := &tls.Config{
+		ClientCAs:  caCertPool,
+		ClientAuth: clientAuth,
+	}
+	http.HandleFunc("/api/v1/events/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+	})
+	server := &http.Server{
+		Addr:      ":8080",
+		TLSConfig: tlsConfig,
+	}
+	return server
+}
+
+func TestMTLSAuthProcessEvent(t *testing.T) {
+	http.DefaultServeMux = &http.ServeMux{}
+	if err := setupTLS(); err != nil {
+		t.Fatalf("Failed to setup TLS due to: %v", err)
+	}
+	defer func() {
+		os.RemoveAll("/tmp/test-pki")
+	}()
+
+	ercOpts := EventsRunnerClientOpts{
+		CaCertPath:     "/tmp/test-pki/ca.crt",
+		ClientKeyPath:  "/tmp/test-pki/client.key",
+		ClientCertPath: "/tmp/test-pki/client.crt",
+		RequestTimeout: time.Minute,
+	}
+	server := setupSetupMockServer(tls.RequireAndVerifyClientCert)
+	go server.ListenAndServeTLS("/tmp/test-pki/server.crt", "/tmp/test-pki/server.key")
+	defer server.Close()
+	erClient, err := NewMTLSClient("https://localhost:8080/api/v1/events/", ercOpts)
+	if err != nil {
+		t.Fatalf("Failed to create MTLS client due to: %v", err)
+	}
+	if err = erClient.ProcessEvent(test_event); err != nil {
+		t.Fatalf("Failed to process event due to: %v", err)
+	}
+}
