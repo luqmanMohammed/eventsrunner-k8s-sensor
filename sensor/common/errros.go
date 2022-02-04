f9a3ba675ec76b705a64343c2a13fbd602b52139
luqmanMohammed <m.luqman077@gmail.com>
2022-02-04 14:38:29 +0530

    Give proper name to error

--- sensor/common/errros.go
+++ sensor/common/errros.go
@@ -4,11 +4,11 @@ import "fmt"
 
 // RequiredFieldMissingError custom error is returned when required field is missing.
 // Missing field is present as part of the error struct
-type RequiredFieldMissingError struct {
-	Field string
+type RequiredConfigMissingError struct {
+	ConfigName string
 }
 
 // Error function implements error interface
-func (rf *RequiredFieldMissingError) Error() string {
-	return fmt.Sprintf("required field %s is missing", rf.Field)
+func (rf *RequiredConfigMissingError) Error() string {
+	return fmt.Sprintf("required config %s is missing", rf.ConfigName)
 }
