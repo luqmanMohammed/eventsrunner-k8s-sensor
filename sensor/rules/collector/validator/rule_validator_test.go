de27d85c276c111dfd344be28f671278a2fb50b2
luqmanMohammed <m.luqman077@gmail.com>
2022-02-10 21:38:44 +0530

    Improve validation tests

--- sensor/rules/collector/validator/rule_validator_test.go
+++ sensor/rules/collector/validator/rule_validator_test.go
@@ -20,6 +20,7 @@ var (
 	ruleNormalized = rules.Rule{
 		ID:         "test-rule",
 		EventTypes: []rules.EventType{rules.EventType("ADDED"), rules.EventType("added"), rules.ADDED},
+		UpdatesOn:  []string{"metadata", "METADATA", "spec"},
 	}
 )
 
@@ -40,5 +41,11 @@ func TestRuleNormalizationAndValidation(t *testing.T) {
 	}
 	if ruleNormalized.EventTypes[0] != rules.ADDED {
 		t.Fatal("Rule should have ADDED event type after normalization")
+	}
+	if len(ruleNormalized.UpdatesOn) != 2 {
+		t.Fatal("Rule should have two updateOn values after normalization")
+	}
+	if ruleNormalized.UpdatesOn[0] != "metadata" {
+		t.Fatal("Rule should have metadata updateOn value after normalization")
 	}
 }
