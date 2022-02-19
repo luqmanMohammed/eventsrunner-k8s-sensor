5799cd320b4762bd2d1522e6b0a88571d051ec2f
luqmanMohammed <m.luqman077@gmail.com>
2022-02-19 23:39:01 +0530

    Add more tests

--- sensor/executor/executor_test.go
+++ sensor/executor/executor_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/luqmanMohammed/eventsrunner-k8s-sensor/sensor/eventqueue"
+	"github.com/luqmanMohammed/eventsrunner-k8s-sensor/sensor/executor/eventsrunner/client"
 )
 
 func TestLogExecutor(t *testing.T) {
@@ -11,8 +12,22 @@ func TestLogExecutor(t *testing.T) {
 	if err == nil {
 		t.Fatalf("Expected error when creating executor of invalid type")
 	}
-
-	executor, err := New(LOG, Opts{})
+	if _, err := New(Type("script"), Opts{
+		ScriptDir:    "/tmp",
+		ScriptPrefix: "script",
+	}); err != nil {
+		t.Fatalf("Failed to create script type executor: %v", err)
+	}
+	if _, err := New(Type("eventsrunner"), Opts{
+		AuthType: "jwt",
+		EventsRunnerClientOpts: client.EventsRunnerClientOpts{
+			EventsRunnerBaseURL: "http://localhost:9090",
+			JWTToken:            "test-token",
+		},
+	}); err != nil {
+		t.Fatalf("Failed to create eventsrunner type executor: %v", err)
+	}
+	executor, err := New(Type("log"), Opts{})
 	if err != nil {
 		t.Fatalf("Failed to create executor: %v", err)
 	}
