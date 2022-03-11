15694d2211dafcf8c62d8019d8e4118a693539f2
Luqman Mohammed <37939871+luqmanMohammed@users.noreply.github.com>
2022-03-12 02:05:53 +0530

    Increase test counts and check near exact wait time (#67)
    * Increase test counts and check near exact wait time
    
    * Increase test counts and starndadize the test
    
    * Generate load based on config

--- integration-tests/integration_test.go
+++ integration-tests/integration_test.go
@@ -24,11 +24,115 @@ import (
 	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
-var cmRuleCount, secretsRuleCount = new(int32), new(int32)
+const (
+	resourceCount = 500
+	interval      = 20 * time.Millisecond
+)
 
-const RESOURCE_COUNT = 100
+var (
+	configYaml = `---
+sensorNamespace: eventsrunner
+executorType: eventsrunner
+authType: jwt
+jwtToken: test-token
+requestTimeout: 10s
+`
 
-func PrepareAndRunJWTBasedMockServer() {
+	ruleConfigYaml = `
+[{
+    "id": "cm-rule-1",
+    "group": "",
+    "version": "v1",
+    "resource": "configmaps",
+    "namespaces": ["k8s-sensor-int-test-ns"],
+    "eventTypes": ["ADDED"]
+    },{
+    "id": "secrets-rule-1",
+    "group": "",
+    "version": "v1",
+    "resource": "secrets",
+    "namespaces": ["k8s-sensor-int-test-ns"],
+    "eventTypes": ["ADDED"]
+}]
+`
+	cpuTotal, memoryTotal, maxCPU, maxMem, runCount = 0, 0, 0, 0, 0
+
+	loadGenerators = []*loadGeneratorConfig{
+		{
+			namespace:     "k8s-sensor-int-test-ns",
+			doneChan:      make(chan struct{}),
+			generator:     configMapLoadGenerator,
+			detectedCount: new(int32),
+			ruleID:        "cm-rule-1",
+			count:         resourceCount,
+			interval:      interval,
+		},
+		{
+			namespace:     "k8s-sensor-int-test-ns",
+			doneChan:      make(chan struct{}),
+			generator:     secretLoadGenerator,
+			detectedCount: new(int32),
+			ruleID:        "secrets-rule-1",
+			count:         resourceCount,
+			interval:      interval,
+		},
+	}
+)
+
+type loadGeneratorConfig struct {
+	namespace     string
+	doneChan      chan struct{}
+	detectedCount *int32
+	ruleID        string
+	count         int
+	interval      time.Duration
+
+	generator func(clientSet *kubernetes.Clientset, id, namespace string, count int, interval time.Duration, doneChan chan<- struct{})
+}
+
+func (c *loadGeneratorConfig) incrementDetectedCountIfRule(ruleName string) {
+	if c.ruleID == ruleName {
+		atomic.AddInt32(c.detectedCount, 1)
+	}
+}
+
+func configMapLoadGenerator(clientSet *kubernetes.Clientset, id, namespace string, count int, interval time.Duration, doneChan chan<- struct{}) {
+	for i := 0; i < count; i++ {
+		configMapName := fmt.Sprintf("configmap-%s-%d", id, i)
+		configMap := &v1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      configMapName,
+				Namespace: namespace,
+			},
+		}
+		_, err := clientSet.CoreV1().ConfigMaps(namespace).Create(context.TODO(), configMap, metav1.CreateOptions{})
+		if err != nil {
+			fmt.Printf("failed to create configmap %s: %v\n", configMapName, err)
+		}
+		time.Sleep(interval)
+	}
+	doneChan <- struct{}{}
+}
+
+func secretLoadGenerator(clientSet *kubernetes.Clientset, id, namespace string, count int, interval time.Duration, doneChan chan<- struct{}) {
+	for i := 0; i < count; i++ {
+		secretName := fmt.Sprintf("secret-%s-%d", id, i)
+		secret := &v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      secretName,
+				Namespace: namespace,
+			},
+		}
+		_, err := clientSet.CoreV1().Secrets(namespace).Create(context.TODO(), secret, metav1.CreateOptions{})
+		if err != nil {
+			fmt.Printf("failed to create secret %s: %v\n", secretName, err)
+		}
+		time.Sleep(interval)
+	}
+	doneChan <- struct{}{}
+}
+
+func prepareAndRunJWTBasedMockServer(lgConfigs []*loadGeneratorConfig) {
 	mockServerMux := http.NewServeMux()
 	mockServerMux.HandleFunc("/api/v1/events", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
@@ -47,14 +151,9 @@ func PrepareAndRunJWTBasedMockServer() {
 					return
 				}
 				ruleID := ruleIDInt.(string)
-				if ruleID == "cm-rule-1" {
-					fmt.Println("ConfigMap Added")
-					atomic.AddInt32(cmRuleCount, 1)
-				} else if ruleID == "secrets-rule-1" {
-					fmt.Println("Secrets Added")
-					atomic.AddInt32(secretsRuleCount, 1)
+				for _, lgConfig := range lgConfigs {
+					lgConfig.incrementDetectedCountIfRule(ruleID)
 				}
-				fmt.Println("Rule ID:", ruleID)
 				w.WriteHeader(http.StatusOK)
 			} else {
 				fmt.Println("Invalid token")
@@ -76,7 +175,7 @@ func PrepareAndRunJWTBasedMockServer() {
 	}
 }
 
-func RunShellCommand(t *testing.T, command string) {
+func runShellCommand(t *testing.T, command string) {
 	cmd := exec.Command("bash", "-c", command)
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
@@ -84,7 +183,7 @@ func RunShellCommand(t *testing.T, command string) {
 	}
 }
 
-func GetCurrentEnvIP(t *testing.T) string {
+func getCurrentEnvIP(t *testing.T) string {
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		t.Fatalf("failed to get interfaces: %v", err)
@@ -109,7 +208,7 @@ func GetCurrentEnvIP(t *testing.T) string {
 	return ""
 }
 
-func WaitForDeploymentToBeReady(t *testing.T, kubeClient *kubernetes.Clientset, namespace string, deploymentName string, retry int) {
+func waitForDeploymentToBeReady(t *testing.T, kubeClient *kubernetes.Clientset, namespace string, deploymentName string, retry int) {
 	for i := 0; i < retry; i++ {
 		deployment, err := kubeClient.AppsV1().Deployments(namespace).Get(context.TODO(), deploymentName, metav1.GetOptions{})
 		if err != nil {
@@ -123,9 +222,7 @@ func WaitForDeploymentToBeReady(t *testing.T, kubeClient *kubernetes.Clientset, 
 	t.Fatalf("deployment %s is not ready", deploymentName)
 }
 
-var cpuMC, memoryMC = make([]int, 0), make([]int, 0)
-
-func CollectSensorResourceUsage(config *rest.Config, readyChan chan struct{}, stopChan chan struct{}) error {
+func collectSensorResourceUsage(config *rest.Config, readyChan chan<- struct{}, stopChan <-chan struct{}) error {
 	metricsClient, err := metricsv.NewForConfig(config)
 	if err != nil {
 		return err
@@ -150,10 +247,19 @@ func CollectSensorResourceUsage(config *rest.Config, readyChan chan struct{}, st
 			cpuUsageMilli, _ := cpuUsage.AsCanonicalBytes(nil)
 			memoryMBInt, _ := strconv.Atoi(string(memoryMB))
 			cpuUsageMilliInt, _ := strconv.Atoi(string(cpuUsageMilli))
-			memoryMC = append(memoryMC, memoryMBInt)
-			cpuMC = append(cpuMC, cpuUsageMilliInt)
-			fmt.Printf("Memory: %v, CPU: %v\n", memoryMB, cpuUsage)
-			fmt.Printf("Metrics CPU Usage: %dm\t Memory Usage: %dMi\n", cpuUsageMilliInt, memoryMBInt)
+
+			memoryTotal += memoryMBInt
+			cpuTotal += cpuUsageMilliInt
+			runCount++
+
+			if memoryMBInt > maxMem {
+				maxMem = memoryMBInt
+			}
+			if cpuUsageMilliInt > maxCPU {
+				maxCPU = cpuUsageMilliInt
+			}
+
+			fmt.Printf("CPU: %dm | Memory: %dMi\n", cpuUsageMilliInt, memoryMBInt)
 		}
 		select {
 		case <-stopChan:
@@ -166,78 +272,30 @@ func CollectSensorResourceUsage(config *rest.Config, readyChan chan struct{}, st
 			break
 		}
 	}
-	avgMem, avgCPU := 0, 0
-	maxCPU, maxMem := 0, 0
-	for _, mem := range memoryMC {
-		if mem > maxMem {
-			maxMem = mem
-		}
-		avgMem += mem
-	}
-	for _, cpu := range cpuMC {
-		if cpu > maxCPU {
-			maxCPU = cpu
-		}
-		avgCPU += cpu
-	}
-	avgMem = avgMem / len(memoryMC)
-	avgCPU = avgCPU / len(cpuMC)
-	fmt.Printf("Average Metrics CPU Usage: %dm\t Average Memory Usage: %dMi\n", avgCPU, avgMem)
-	fmt.Printf("Max Metrics CPU Usage: %dm\t Max Memory Usage: %dMi\n", maxCPU, maxMem)
+	avgMem := memoryTotal / runCount
+	avgCPU := cpuTotal / runCount
+	fmt.Printf("Average CPU: %dm\t Average Memory: %dMi\n", avgCPU, avgMem)
+	fmt.Printf("Max CPU: %dm\t Max Memory Usage: %dMi\n", maxCPU, maxMem)
+	readyChan <- struct{}{}
 	return nil
-}
-
-var configYaml = `---
-sensorNamespace: eventsrunner
-executorType: eventsrunner
-authType: jwt
-jwtToken: test-token
-requestTimeout: 10s
-`
-
-var ruleConfigYaml = `
-[{
-    "id": "cm-rule-1",
-    "group": "",
-    "version": "v1",
-    "resource": "configmaps",
-    "namespaces": ["k8s-sensor-int-test-ns"],
-    "eventTypes": ["ADDED"]
-    },{
-    "id": "secrets-rule-1",
-    "group": "",
-    "version": "v1",
-    "resource": "secrets",
-    "namespaces": ["k8s-sensor-int-test-ns"],
-    "eventTypes": ["ADDED"]
-}]
-`
-
-var sensorDeployment = appsv1.Deployment{
-	ObjectMeta: metav1.ObjectMeta{
-		Name:      "eventsrunner-k8s-sensor",
-		Namespace: "eventsrunner",
-	},
-	Spec: appsv1.DeploymentSpec{
-		Replicas: &[]int32{1}[0],
-	},
 }
 
 func TestIntegration(t *testing.T) {
 
+	// Skip test if INT_TEST flag is not provided
 	if os.Getenv("INT_TEST") != "true" {
 		t.Skip("skipping integration test")
 	}
 
-	go PrepareAndRunJWTBasedMockServer()
+	// Run JWT Based mock server in another routine
+	go prepareAndRunJWTBasedMockServer(loadGenerators)
 
 	// Setting up prerequisites
-	RunShellCommand(t, "kubectl create -f prerequisite-k8s-resources.yaml")
-	defer RunShellCommand(t, "kubectl delete -f prerequisite-k8s-resources.yaml")
+	runShellCommand(t, "kubectl create -f prerequisite-k8s-resources.yaml")
+	defer runShellCommand(t, "kubectl delete -f prerequisite-k8s-resources.yaml")
 
 	// Get Test Environment IP
-	ip := GetCurrentEnvIP(t)
-
+	ip := getCurrentEnvIP(t)
 	// Create Config Map setting eventsRunner config
 	configYaml += "eventsRunnerBaseURL: http://" + ip + ":9090"
 	configMap := v1.ConfigMap{
@@ -274,16 +332,15 @@ func TestIntegration(t *testing.T) {
 	}
 	defer clientSet.CoreV1().ConfigMaps("eventsrunner").Delete(context.Background(), rulesConfigYaml.Name, metav1.DeleteOptions{})
 
+	// Get image tag from IMAGE_TAG env. Will be populated by CI
 	imageTag := os.Getenv("IMAGE_TAG")
 	if imageTag == "" {
 		imageTag = "latest"
 	}
 	image := "luqmanmohammed/eventsrunner-k8s-sensor:" + imageTag
 	t.Logf("Running eventsrunner-k8s-sensor with image %s", image)
-
 	// Read deployment template yaml from senor-deployment.yml file
 	var deployment appsv1.Deployment
-
 	deploymentJSON, err := ioutil.ReadFile("sensor-deployment.json")
 	if err != nil {
 		t.Fatalf("failed to read deployment template file: %v", err)
@@ -293,7 +350,6 @@ func TestIntegration(t *testing.T) {
 		t.Fatalf("failed to unmarshal deployment template file: %v", err)
 	}
 	deployment.Spec.Template.Spec.Containers[0].Image = image
-
 	// Create sensor deployment
 	_, err = clientSet.AppsV1().Deployments("eventsrunner").Create(context.Background(), &deployment, metav1.CreateOptions{})
 	if err != nil {
@@ -301,16 +357,16 @@ func TestIntegration(t *testing.T) {
 	}
 	defer clientSet.AppsV1().Deployments("eventsrunner").Delete(context.Background(), deployment.Name, metav1.DeleteOptions{})
 	// Wait for deployment to be ready
-	WaitForDeploymentToBeReady(t, clientSet, "eventsrunner", deployment.Name, 30)
+	waitForDeploymentToBeReady(t, clientSet, "eventsrunner", deployment.Name, 30)
 
+	// Setup monitoring
 	metricsReadyChan := make(chan struct{})
 	metricsStopChan := make(chan struct{})
 	go func() {
-		if err := CollectSensorResourceUsage(kubeconfig, metricsReadyChan, metricsStopChan); err != nil {
+		if err := collectSensorResourceUsage(kubeconfig, metricsReadyChan, metricsStopChan); err != nil {
 			panic(err)
 		}
 	}()
-
 	select {
 	case <-metricsReadyChan:
 		t.Log("Metrics are ready")
@@ -318,55 +374,35 @@ func TestIntegration(t *testing.T) {
 		t.Fatalf("failed to initialize sensor resource usage")
 	}
 
-	cmDone, secretDone := make(chan struct{}), make(chan struct{})
-	go func(done chan struct{}) {
-		for i := 0; i < RESOURCE_COUNT; i++ {
-			// Create configmap
-			testCM := v1.ConfigMap{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "cm-" + strconv.Itoa(i),
-					Namespace: "k8s-sensor-int-test-ns",
-				},
-				Data: map[string]string{},
-			}
-			_, err = clientSet.CoreV1().ConfigMaps("k8s-sensor-int-test-ns").Create(context.Background(), &testCM, metav1.CreateOptions{})
-			if err != nil {
-				fmt.Printf("failed to create configmap: %v\n", err)
-			}
-			time.Sleep(time.Millisecond * 100)
-		}
-		close(done)
-	}(cmDone)
+	for i, config := range loadGenerators {
+		go config.generator(clientSet, strconv.Itoa(i), config.namespace, config.count, config.interval, config.doneChan)
+	}
 
-	go func(done chan struct{}) {
-		for i := 0; i < RESOURCE_COUNT; i++ {
-			testSecret := v1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "secret-" + strconv.Itoa(i),
-					Namespace: "k8s-sensor-int-test-ns",
-				},
-				Data: map[string][]byte{},
-			}
-			_, err = clientSet.CoreV1().Secrets("k8s-sensor-int-test-ns").Create(context.Background(), &testSecret, metav1.CreateOptions{})
-			if err != nil {
-				fmt.Printf("failed to create secret: %v\n", err)
-			}
-			time.Sleep(time.Millisecond * 100)
-		}
-		close(done)
-	}(secretDone)
+	for _, config := range loadGenerators {
+		<-config.doneChan
+	}
 
-	<-cmDone
-	<-secretDone
+	// Calculate how much time is required extra to finish processing
+	startTime := time.Now()
+
+	for {
+		processedGenerators := 0
+		proccessedItems := 0
+		for _, config := range loadGenerators {
+			if config.count == int(*config.detectedCount) {
+				processedGenerators++
+				proccessedItems += int(*config.detectedCount)
+			}
+		}
+		if processedGenerators == len(loadGenerators) {
+			t.Logf("All generators processed %d items", proccessedItems)
+			t.Logf("Extra time taken after last event was created %v", time.Since(startTime))
+			break
+		}
+		if time.Since(startTime) > time.Minute*1 {
+			t.Fatal("Failed to process all events within extra minute")
+		}
+	}
 	metricsStopChan <- struct{}{}
-
-	time.Sleep(time.Second * 15)
-
-	if *cmRuleCount != int32(RESOURCE_COUNT) {
-		t.Fatalf("expected 10 configmaps, got %d", *cmRuleCount)
-	}
-
-	if *secretsRuleCount != int32(RESOURCE_COUNT) {
-		t.Fatalf("expected 10 secrets, got %d", *secretsRuleCount)
-	}
+	<-metricsReadyChan
 }
